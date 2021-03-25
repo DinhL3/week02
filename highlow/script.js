1c9862d61562e9f7ce5b8f29a7979a9326c7275d
Dinh Le <lqdquangdinh@gmail.com>
2021-03-25 22:54:57 +0700

    continue working on this tomorrow

---
+++ highlow/script.js
@@ -0,0 +1,9 @@
+// Variables
+let myMoney = 100
+let message = document.getElementById('message')
+
+
+function gameStart() {
+    message.innerHTML = "Good luck!"
+    document.getElementById('startDiv').style.display = 'none'
+}
